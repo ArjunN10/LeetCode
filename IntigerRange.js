30f1644e8f710fdc3f386ed3e7fe80c15f1d3498
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-12-06 09:30:18 +0530

    second commit

--- IntigerRange.js
+++ IntigerRange.js
@@ -1,7 +1,3 @@
-//Question:- You are given a 2D integer array ranges and two integers left and right. Each ranges[i] = [starti, endi] represents an inclusive 
-//interval between starti and endi.Return true if each integer in the inclusive range [left, right] is covered by at least one interval in ranges.
-//Return false otherwise.An integer x is covered by an interval ranges[i] = [starti, endi] if starti <= x <= endi.
-
 
 
 
